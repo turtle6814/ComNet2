475086b2cd9f64c046c8758b0f55e1312b7caaf7
turtle6814 <minhtringuyen14092003@gmail.com>
2025-02-20 16:01:02 +0700

    add print function

--- src/test.java
+++ src/test.java
@@ -6,11 +6,15 @@ public class test {
         return result;
     }
 
+    public static void printData(int n) {
+        System.out.printf("The result is: %d", n*n);
+    }
 
     public static void main(String[] args) {
         System.out.println("Hi, my name is John Doe!");
-        System.err.println("XXX");
+        System.out.println("XXX");
         int n = 5;
-        System.err.println(solve(n));
+        System.out.println(solve(n));
+        printData(n);
     }
 }
