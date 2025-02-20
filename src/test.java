30ba4e7558f53bd88ee51a1103285160bd471b17
turtle6814 <minhtringuyen14092003@gmail.com>
2025-02-20 14:36:51 +0700

    add test function

--- src/test.java
+++ src/test.java
@@ -1,5 +1,15 @@
 public class test {
+
+    public static int solve(int n) {
+        int result = n*10;
+        
+        return result;
+    }
+
+
     public static void main(String[] args) {
         System.out.println("Hi, my name is John Doe!");
+        int n = 5;
+        System.err.println(solve(n));
     }
 }
